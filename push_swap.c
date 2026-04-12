f28cd0e0ff7e5f5752f4b1680f92dfe095af0d21
Monya <seemslikeweonlygobackwards@gmail.com>
2026-04-12 22:09:44 +0400

    Add files via upload
    added swap and push functions to push_swap.c

--- push_swap.c
+++ push_swap.c
@@ -3,15 +3,38 @@
 /*                                                        :::      ::::::::   */
 /*   push_swap.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: amghazar <amghazar@student.42.fr>          +#+  +:+       +#+        */
+/*   By: monika <monika@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/09 23:23:07 by amghazar          #+#    #+#             */
-/*   Updated: 2026/04/11 17:08:26 by amghazar         ###   ########.fr       */
+/*   Updated: 2026/04/12 22:06:28 by monika           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "stdio.h"
 #include "libft/libft.h"
+
+void	swap(int *stack, int size)
+{
+	int	temp;
+	
+	if (size < 2)
+		return ;
+	temp = stack[size - 1];
+	stack[size - 1] = stack[size -2];
+	stack[size - 2] = temp;
+}
+
+void	push(int *stack1,int *stack2, int *size1, int *size2)
+{
+	int temp;
+
+	if (*size2 < 1)
+		return ;
+	temp = stack2[*size2 - 1];
+	(*size2)--;
+	(*size1)++;
+	stack1[*size1 - 1] = temp;
+}
 
 void	rotate(int **stack, int size)
 {
